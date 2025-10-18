67a86706d0a60650e41da7a65a12f4177c59acec
Your Name <you@example.com>
2025-10-18 16:28:49 +0000

    new commit zz

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import "materialize-css/dist/js/materialize.min.js";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { AuthProvider } from "./auth";
 import { CartProvider } from "./cart";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <AuthProvider>
       <CartProvider>
         <App />
       </CartProvider>
     </AuthProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
